94aaa7f7f4b9f611d7034ee9feeb3dc408dd0126
sahir <sahirde@gmail.com>
2024-01-29 00:31:22 +0100

    favi

--- vite.config.js
+++ vite.config.js
@@ -11,18 +11,18 @@ export default defineConfig({
     VitePWA({
       includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
-        name: 'My Awesome App',
-        short_name: 'MyApp',
-        description: 'My Awesome App description',
+        name: 'Where Am I Next?',
+        short_name: 'Where Am I Next?',
+        description: 'Where is your next class at UvA?',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: 'android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'pwa-512x512.png',
+            src: 'android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
