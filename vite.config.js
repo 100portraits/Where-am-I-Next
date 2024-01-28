8fd3ca159a35257cc1da98b7b3dd77985ae44397
sahir <sahirde@gmail.com>
2024-01-29 00:24:40 +0100

    favi

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), 
     VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
-   })],
+    })
+  ],
 })
